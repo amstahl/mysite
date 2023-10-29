19678b4c81a59029f54551330c40d1f4b71d28a9
Alyson <amstahl@ucsc.edu>
2023-10-29 17:58:57 -0600

    deleted and recreated polls app to fix url issues

--- Polls/apps.py
+++ Polls/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class PollsConfig(AppConfig):
-    name = 'Polls'
+class pollsConfig(AppConfig):
+    name = 'polls'
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,15 +16,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-# Uncomment next two lines to enable admin:
-#from django.contrib import admin
-#from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
-from django.urls import include, re_path
-import Polls.views
-
-# Django processes URL patterns in the order they appear in the array
 urlpatterns = [
-    re_path(r'^$', Polls.views.index, name='index'),
-    re_path(r'^home$', Polls.views.index, name='home')
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
